747b3f58f0b9fa80e14809b6e01686f2a00cb4fc
Orob1531 <132944332+Orob1531@users.noreply.github.com>
2023-05-15 16:10:20 -0300

    discord_code.py

--- discord_code.py
+++ discord_code.py
@@ -5,244 +5,173 @@
 ```python
 
 """
-
-Este es un bot de Discord que realiza funciones básicas de moderación.
-
-Incluye comandos para expulsar, banear y desbanear a usuarios del servidor.
-
-También hay un comando para borrar mensajes de un canal en particular.
-
+Este es un bot de Discord que tiene un solo comando slash, 
+/ping. El comando responde con un mensaje que muestra la latencia del bot.
 """
-
 import os
-
-import discord
-
-from discord.ext import commands
-
+import disnake
+from disnake.ext import commands
 from dotenv import load_dotenv
-
-# Cargar las variables de entorno desde el archivo `.env`
 
 load_dotenv()
 
-# Configurar el bot
-
-intents = discord.Intents.default()
-
+intents = disnake.Intents.default()
 intents.members = True
-
 intents.messages = True
+intents.guilds = True
+intents.reactions = True
+intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-# Variables de autenticación
-
-CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
-
-CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
-
-BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-
-# Autenticar el bot
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 @bot.event
-
 async def on_ready():
-
     """
+    Esta función se ejecuta cuando el bot se conecta al servidor de Discord.
 
-    Imprime un mensaje cuando el bot esté listo y conectado al servidor.
-
-    """
-
-    print(f'Conectado como: {bot.user.name} - {bot.user.id}')
-
-# Imprime un mensaje cuando el bot esté listo y conectado al servidor
-
-# Comando de kick
-
-@bot.command()
-
-@commands.has_permissions(kick_members=True)
-
-async def kick(ctx, member: discord.Member, *, reason=None):
-
-    """
-
-    Expulsa a un usuario del servidor.
-
-    Parameters:
-
-    - member: El miembro del servidor que se va a expulsar.
-
-    - reason (opcional): La razón detrás de la expulsión.
+    Parametros:
+    - Ninguno.
 
     Returns:
-
-    - Un mensaje que indica que el usuario ha sido expulsado.
+    - Ninguno.
 
     Raises:
-
-    - discord.ext.commands.errors.BadArgument: Si se proporciona un miembro inválido.
-
-    - discord.ext.commands.errors.MissingPermissions:
-
-    Si el autor del comando no tiene permiso para expulsar miembros.
-
+    - Ninguno.
     """
+    print(f"Conectado como {bot.user} ({bot.user.id})")
 
-    await member.kick(reason=reason)
-
-    await ctx.send(f'Usuario {member} expulsado. Razón: {reason}')
-
-# Comando de ban
-
-@bot.command()
-
-@commands.has_permissions(ban_members=True)
-
-async def ban(ctx, member: discord.Member, *, reason=None):
-
+@bot.slash_command(
+    name="ping",
+    description="Muestra el ping del bot."
+)
+async def ping(ctx: disnake.ApplicationCommandInteraction):
     """
-
-    Banea a un usuario del servidor.
+    Muestra el ping del bot.
 
     Parameters:
+    - ctx: El contexto del comando.
 
+    Returns:
+    - Ninguno.
+
+    Raises:
+    - Ninguno.
+    """
+    await ctx.response.send_message(f"Pong! Latencia: {bot.latency*1000:.2f} ms")
+
+@bot.slash_command(
+    name="kick",
+    description="Expulsa a un miembro del servidor."
+)
+@commands.has_permissions(kick_members=True)
+async def kick(ctx: disnake.ApplicationCommandInteraction,
+        member: disnake.Member, *, reason: str = "No se especificó una razón."):
+    """
+    Expulsa a un miembro del servidor.
+
+    Parameters:
+    - ctx: El contexto del comando.
+    - member: El miembro del servidor que se va a expulsar.
+    - Razon (opcional): La razón detrás de la expulsión.
+
+    Returns:
+    - Ninguno
+
+    Raises:
+    - discord.ext.commands.errors.BadArgument: Si se proporciona un miembro inválido.
+    - discord.ext.commands.errors.MissingPermissions:
+    Si el autor del comando no tiene permiso para expulsar miembros.
+    """
+    await member.kick(reason=reason)
+    await ctx.response.send_message(f"El \
+            miembro {member.mention} ha sido expulsado del servidor. Razón: {reason}")
+@bot.slash_command(
+    name="ban",
+    description="Banea a un miembro del servidor."
+)
+@commands.has_permissions(ban_members=True)
+async def ban(ctx: disnake.ApplicationCommandInteraction,
+            member: disnake.Member, *, reason: str = "No se especificó una razón."):
+    """
+    Banea a un miembro del servidor.
+
+    Parameters:
+    - ctx: El contexto del comando.
     - member: El miembro del servidor que se va a banear.
-
     - reason (opcional): La razón detrás del baneo.
 
     Returns:
-
-    - Un mensaje que indica que el usuario ha sido baneado.
+    - Ninguno
 
     Raises:
-
     - discord.ext.commands.errors.BadArgument: Si se proporciona un miembro inválido.
-
     - discord.ext.commands.errors.MissingPermissions:
-
     Si el autor del comando no tiene permiso para banear miembros.
-
     """
-
     await member.ban(reason=reason)
-
-    await ctx.send(f'Usuario {member} baneado. Razón: {reason}')
-
-# Comando de unban
-
-@bot.command()
-
+    await ctx.response.send_message(f"El\
+            miembro {member.mention} ha sido baneado del servidor. Razón: {reason}")
+@bot.slash_command(
+    name="unban",
+    description="Desbanea a un miembro del servidor."
+)
 @commands.has_permissions(ban_members=True)
-
-async def unban(ctx, *, member):
-
+async def unban(ctx: disnake.ApplicationCommandInteraction,
+            member: str, *, reason: str = "No se especificó una razón."):
     """
-
-    Desbanea a un usuario del servidor.
+    Desbanea a un miembro del servidor.
 
     Parameters:
-
+    - ctx: El contexto del comando.
     - member: El nombre y discriminador (ejemplo: "usuario#0000") del usuario a desbanear.
+    - reason (opcional): La razón detrás del desbaneo.
 
     Returns:
-
-    - Un mensaje que indica que el usuario ha sido desbaneado.
+    - Ninguno
 
     Raises:
-
     - discord.ext.commands.errors.BadArgument: Si se proporciona un miembro inválido.
-
     - discord.ext.commands.errors.NotFound: Si la persona que se intenta desbanear no está baneada.
-
     - discord.ext.commands.errors.MissingPermissions: 
-
     Si el autor del comando no tiene permiso para desbanear miembros.
-
     """
-
     banned_users = await ctx.guild.bans()
-
-    member_name, member_discriminator = member.split('#')
+    member_name, member_discriminator = member.split("#")
 
     for ban_entry in banned_users:
-
         user = ban_entry.user
 
         if (user.name, user.discriminator) == (member_name, member_discriminator):
-
-            await ctx.guild.unban(user)
-
-            await ctx.send(f'Usuario {user.name} desbaneado')
-
+            await ctx.guild.unban(user, reason=reason)
+            await ctx.response.send_message(f"El\
+                    miembro {user.mention} ha sido desbaneado del servidor. Razón: {reason}")
             return
 
-    raise commands.BadArgument(f'Usuario {member} no encontrado en la lista de baneos')
-
-# Comando de limpiar mensajes
-
-@bot.command()
-
+    await ctx.send(f"No se encontró un usuario baneado con el nombre {member}.")
+@bot.slash_command(
+    name="clear",
+    description="Elimina un número específico de mensajes de un canal."
+)
 @commands.has_permissions(manage_messages=True)
-
-async def clear(ctx, amount=5):
-
+async def clear(ctx: disnake.ApplicationCommandInteraction, amount: int):
     """
-
-    Elimina una cantidad específica de mensajes del canal actual.
+    Elimina un número específico de mensajes de un canal.
 
     Parameters:
-
-    - amount (opcional): El número de mensajes que se eliminarán. Por defecto son 5.
+    - ctx: El contexto del comando.
+    - amount: El número de mensajes que se eliminarán.
 
     Returns:
-
-    - Un mensaje que indica que los mensajes se han eliminado.
+    - Ninguno.
 
     Raises:
-
     - discord.ext.commands.errors.MissingPermissions: 
-
     Si el autor del comando no tiene permiso para eliminar mensajes.
-
     """
-
     await ctx.channel.purge(limit=amount + 1)
-
-    await ctx.send(f'{amount} mensajes han sido borrados')
-
-# Manejador de errores
-
-@bot.event
-
-async def on_command_error(ctx, error):
-
-    """
-
-    Maneja los errores que ocurren al ejecutar un comando.
-
-    Parameters:
-
-    - ctx: El contexto del comando que se intentó ejecutar.
-
-    - error: El error que se levantó al ejecutar el comando.
-
-    Returns:
-
-    - Un mensaje de error que indica que se produjo un error al ejecutar el comando.
-
-    Raises:
-
-    - No lanza ninguna excepción.
-
-    """
-
-    if isinstance(error, commands.CommandNotFound):
-
-        await ctx.send('Comando no encontrado')
+    await ctx.response.send_message(f"{amount} mensajes han sido eliminados.")
 
 # Inicio del bot
 
